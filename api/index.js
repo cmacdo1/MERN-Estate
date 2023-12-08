721694f1a97c0ffbda6ceac5439a86002d3a4740
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2023-12-08 10:35:16 -0700

    created sign up api route & middleware

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,6 +23,7 @@ mongoose
 
 /* ROUTES */
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
