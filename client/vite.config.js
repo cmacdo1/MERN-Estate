8eb10bdcd00028ad8531cbfe322685af08c87b54
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2023-12-09 11:41:18 -0700

    completed sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        /* everything starting with /api will add this prefix to the beginning */
+        target: 'http://localhost:3000',
+        secure: false,
+      }
+    }
+  }
 })
