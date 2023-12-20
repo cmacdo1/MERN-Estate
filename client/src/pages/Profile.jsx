61b72431a32c44a914f6edf948ee3462d2d1fc9e
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2023-12-19 21:38:29 -0700

    completed create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,6 +21,7 @@ import {
   signOutUserStart,
 } from '../redux/user/userSlice.js';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -188,12 +189,20 @@ export default function Profile() {
           className='border p-3 rounded-lg'
           onChange={handleChange}  
         />
+
+        {/* BUTTONS */}
         <button
           disabled={loading} 
           className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
         >
             {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link 
+          className='bg-green-700 text-white rounded-lg p-3 uppercase text-center hover:opacity-95' 
+          to={'/create-listing'}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span
